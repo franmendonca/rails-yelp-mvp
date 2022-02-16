d329133435374229ca3f92635f7ec08514cd65ef
Francisco Mendonca <franciscomendonca8@gmail.com>
2022-02-16 13:11:13 +0000

    created index and show page with links

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,16 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   RESTAURANTS = %w(chinese italian japanese french belgian)
   validates :category, inclusion: { in: RESTAURANTS }
-  validates :name, :address, :phone_number, presence: true
+  validates :name, :address, :category, presence: true
 end
