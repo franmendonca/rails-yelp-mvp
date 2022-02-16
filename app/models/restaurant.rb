2181858cc5d4972337c34b000520bfbe74a6a4fe
Francisco Mendonca <franciscomendonca8@gmail.com>
2022-02-16 12:30:08 +0000

    added validations to models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  RESTAURANTS = %w(chinese italian japanese french belgian)
+  validates :category, inclusion: { in: RESTAURANTS }
+  validates :name, :address, :phone_number, presence: true
 end
