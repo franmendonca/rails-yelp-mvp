46ccb83e3698356a775bd16119702634a4e38ff4
Francisco Mendonca <franciscomendonca8@gmail.com>
2022-02-16 15:09:42 +0000

    added a new tab

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,10 +7,28 @@ class RestaurantsController < ApplicationController
 
   def show; end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
   end
 end
